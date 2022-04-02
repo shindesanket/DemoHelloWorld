b6a75b6682c31161bb78c2fe350008935c9877c8
Sanket Shinde <sanketshinde7@gmail.com>
2022-04-02 08:37:01 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Welcome to Simplilearn");
+	System.out.println("Welcome to Simplilearn for second time");
   }
 }
 
